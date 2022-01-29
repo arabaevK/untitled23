f8439209769c0848bf99b63e11b460fed25b6402
arabaevK <arabaev16@mail.ru>
2022-01-29 19:44:15 +0600

    Initial commit first

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(generateRanodmAge());
+        System.out.println(rec(generateRanodmAge(), 24));
+        System.out.println("-------------------------");
+        System.out.println(rec(-45, 30));
+        System.out.println("-------------------------");
+
+        System.out.println(rec(25, 40));
+        System.out.println("-------------------------");
+
+        System.out.println(rec(-20, 15));
+        System.out.println("-------------------------");
+
+        System.out.println(rec(80, 50));
+
+
+    }
+
+    public static String rec(int temperature, int age) {
+        String toy = "Можно идти гулять";
+        String toy1 = "нельзя идти гулять";
+        if (age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) {
+            return toy;
+        } else if (age < 20 && temperature >= 0 && temperature <= 28) {
+            return toy;
+
+        } else if (age > 45 && temperature > -10 && temperature <= 35) {
+            return toy;
+
+        } else {
+            System.out.println(toy1);
+
+        }
+        return toy1;
+    }
+    public static int generateRanodmAge(){
+        Random random = new Random();
+        int randomNum = random.nextInt(100);
+        return randomNum;
+    }
+}
